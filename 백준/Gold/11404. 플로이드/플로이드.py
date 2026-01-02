4cc3c7fe2eded854dad4afe929e9e7ff743228bf
SeongChoel <83538651+SeongChoel@users.noreply.github.com>
2026-01-02 14:43:06 +0900

    [Gold IV] Title: 플로이드, Time: 472 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+graph = [[1e9] * (n + 1) for i in range(n + 1)]
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    if graph[a][b] > c:
+        graph[a][b] = c
+
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if graph[i][j] == 1e9:
+            print(0, end=" ")
+        else:
+            print(graph[i][j], end=" ")
+    print()
